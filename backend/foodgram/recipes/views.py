9a1845e60e70e2e7200c43efd115597924324c36
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-10-26 14:10:08 +0300

    Add pdf_gen function for download_shopping_cart endpoint

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -1,10 +1,18 @@
+import io
+
 from django.db.models import Sum
-from django.http import HttpResponse
+from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
 
 from foodgram.permissions import AuthorOnly
 
@@ -76,6 +84,30 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 status=status.HTTP_400_BAD_REQUEST
                 )
 
+    def pdf_gen(self, ingredients_annotate):
+        buf = io.BytesIO()
+        c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+        pdfmetrics.registerFont(TTFont('FreeSans', 'FreeSans.ttf'))
+        textobj = c.beginText()
+        textobj.setTextOrigin(inch, inch)
+        textobj.setFont('FreeSans', 14)
+
+        ingredients_list = ['Cписок покупок:']
+
+        for i in ingredients_annotate:
+            ingredients_list.append(f"{i['ingredient__name']}"
+                                    f"({i['ingredient__measurement_unit']}) - "
+                                    f"{i['sum_amount']}")
+        for obj in ingredients_list:
+            textobj.textLine(obj)
+
+        c.drawText(textobj)
+        c.showPage()
+        c.save()
+        buf.seek(0)
+
+        return buf
+
     @action(methods=['GET'], detail=False)
     def download_shopping_cart(self, request):
         shopping_cart = ShoppingCart.objects.filter(user=request.user)
@@ -85,18 +117,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         ingredients_annotate = ingredients.values(
             'ingredient__name', 'ingredient__measurement_unit').annotate(
             sum_amount=Sum('amount'))
-        ingredient_list = 'Cписок покупок:'
 
-        for num, i in enumerate(ingredients_annotate):
-            ingredient_list += (
-                f"\n{i['ingredient__name']}"
-                f"({i['ingredient__measurement_unit']}) - "
-                f"{i['sum_amount']}"
-            )
-        response = HttpResponse(
-            ingredient_list, 'Content-Type: application/pdf'
-        )
-        response['Content-Disposition'] = 'attachment; filename="shopping_cart.pdf"'
+        pdf_ingredients_list = self.pdf_gen(ingredients_annotate)
+
+        response = FileResponse(pdf_ingredients_list,
+                                as_attachment=True,
+                                filename='shopping_cart.pdf',)
         return response
 
     @action(methods=['POST', 'DELETE'], detail=True)
