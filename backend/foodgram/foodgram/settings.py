8d41cefdb0ccf1eff149c3695227c9508ec8e3b5
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-11-02 13:34:04 +0300

    fix docker-compose.yml

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -133,11 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/backend_media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
 
 STATIC_URL = '/backend_static/'
-STATIC_ROOT = BASE_DIR / 'backend_static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
 
 # Default primary key field type
