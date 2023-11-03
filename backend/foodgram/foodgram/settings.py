1bdf7c173ad0e452302f1b0fb2a9e61b42f9d1af
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-11-03 12:41:50 +0300

    Change directory for static files in docker-compose.yml

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-MEDIA_URL = '/backend_media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
-STATIC_URL = '/backend_static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
 
