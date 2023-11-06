c8d6967067259929d6e3d39e4e80a25e61912b23
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-11-06 21:36:47 +0300

    Fix bugs

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -103,8 +103,8 @@ DJOSER = {
         False,
     },
     'PERMISSIONS': {
-        'user': 'rest_framework.permissions.AllowAny',
-        'user_list': 'rest_framework.permissions.AllowAny',
+        'user': 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'user_list': 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     }
 }
 
