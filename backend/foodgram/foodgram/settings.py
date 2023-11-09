fd675f53643f9908c1152cf328bbcdd846a46ef5
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-11-10 00:26:47 +0300

    Fix bugs

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -105,8 +105,6 @@ DJOSER = {
 }
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
