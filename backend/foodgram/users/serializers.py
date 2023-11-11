c4f06216ad37985977fc020825ca41fb163e718e
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-11-12 00:32:42 +0300

    Fix get_recipes function in Subscription serializer

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -56,7 +56,7 @@ class SubscriptionRetrieveSerializer(CustomUserRetrieveSerializer):
         '''
         Method for getting author's recipes with parameter 'recipes_limit'.
         '''
-        recipes = obj.subscription_author.recipe_author.all()
+        recipes = obj.author.recipe_author.all()
         request = self.context.get('request')
         recipes_limit = request.GET.get('recipes_limit')
         if recipes_limit:
