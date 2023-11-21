e04d3a2f5f2c31b70da608093bbc05745658db8a
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-11-21 16:09:05 +0300

    Fix permissin for users list

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -103,9 +103,9 @@ DJOSER = {
     'HIDE_USERS': {
         False,
     },
-    'PERMISSIONS': {
-        'user_list': ['rest_framework.permissions.AllowAny'],
-    },
+    # 'PERMISSIONS': {
+    #     'user_list': ['rest_framework.permissions.AllowAny'],
+    # },
 }
 
 REST_FRAMEWORK = {
