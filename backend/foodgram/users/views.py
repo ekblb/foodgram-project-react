d73a4391f7d7cde0a5e3f45cc74649be451acba4
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-11-22 16:06:23 +0300

    Add in subscription serializer model name

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -7,7 +7,8 @@ from rest_framework.response import Response
 
 from .models import CustomUser, Subscription
 from .serializers import (SubscriptionRetrieveSerializer,
-                          SubscriptionCreateDeleteSerializer)
+                          SubscriptionCreateDeleteSerializer,
+                          CustomUserRetrieveSerializer)
 
 
 class CustomUserViewSet(UserViewSet):
@@ -16,6 +17,7 @@ class CustomUserViewSet(UserViewSet):
     '''
     queryset = CustomUser.objects.all()
     permission_classes = (permissions.AllowAny, )
+    serializer_class = CustomUserRetrieveSerializer
 
     @action(methods=['GET'], detail=False,
             permission_classes=[permissions.IsAuthenticated],
