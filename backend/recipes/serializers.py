7eb4feee692f5a01aa0e7995018581a200ffeec8
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-12-15 17:30:26 +0300

    Fix FavoriteRecipeSerializer

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -24,7 +24,9 @@ class FavoriteRecipeSerializer(serializers.ModelSerializer):
         """
         Method for getting response after adding recipe.
         """
-        serializer = RecipeSerializer(instance, context=self.context)
+        request = self.context.get('request')
+        serializer = RecipeSerializer(instance.recipe,
+                                      context={'request': request})
         return serializer.data
 
 
