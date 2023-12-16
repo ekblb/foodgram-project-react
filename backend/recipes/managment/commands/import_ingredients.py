986f010e384401a7fc03d6e86b3b93a842cb560a
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-12-16 15:54:20 +0300

    New

---
+++ backend/recipes/managment/commands/import_ingredients.py
@@ -0,0 +1,20 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    """
+    Class for loading ingredients.csv to the database.
+    """
+    help = 'Load ingredients.csv to the database'
+
+    def handle(self, *args, **options):
+        with open('data/ingredients.csv', encoding='utf-8') as file:
+            file_reader = csv.reader(file)
+            for row in file_reader:
+                name, measurement_unit = row
+                Ingredient.objects.get_or_create(
+                    name=name, measurement_unit=measurement_unit)
