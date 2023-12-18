ea8cc65183252ff41816d1a73c5039116bfc06dc
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-12-18 13:26:12 +0300

    Fix serializers and views in recipes app

--- backend/recipes/management/commands/import_ingredients.py
+++ backend/recipes/management/commands/import_ingredients.py
@@ -15,6 +15,4 @@ class Command(BaseCommand):
         with open('recipes/data/ingredients.csv', encoding='utf-8') as file:
             file_reader = csv.reader(file)
             for row in file_reader:
-                name, measurement_unit = row
-                Ingredient.objects.bulk_create(
-                    name=name, measurement_unit=measurement_unit)
+                Ingredient.objects.bulk_create(**row)
